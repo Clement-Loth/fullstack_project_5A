eb56077658623da639d7db203839696c4c0cc2d2
clement <clement.loth1@gmail.com>
2022-09-30 16:31:01 +0200

    Last fix for repos i hope ...

--- covid-api/src/main/java/org/polytech/covid/repository/AppointmentRepository.java
+++ covid-api/src/main/java/org/polytech/covid/repository/AppointmentRepository.java
@@ -1,8 +1,13 @@
 package org.polytech.covid.repository;
 
+import java.util.Date;
+import java.util.List;
+
 import org.polytech.covid.entity.Appointment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AppointmentRepository extends JpaRepository<Appointment, Long>{
-
+    List<Appointment> findByFirstName(String firstName);
+    List<Appointment> findByLastName(String lastName);
+    List<Appointment> findByDate(Date date);
 }
