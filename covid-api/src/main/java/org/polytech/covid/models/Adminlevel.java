2c8f382c16864bbc05f26d39fe764cc2b4be0e4f
jimmy789521 <71819826+jimmy789521@users.noreply.github.com>
2022-09-30 14:13:51 +0200

    Added modeles
    Model for Center and AdminLevel

---
+++ covid-api/src/main/java/org/polytech/covid/models/Adminlevel.java
@@ -0,0 +1,11 @@
+package db.modeles;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Adminlevel {
+    private int id;
+    private String name;
+    private String role;
+
+}
---
+++ covid-api/src/main/java/org/polytech/covid/models/Center.java
@@ -0,0 +1,12 @@
+package db.modeles;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Center {
+    private int id;
+    private String city;
+    private String name;
+    private String location;
+
+}
