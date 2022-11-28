71aed114c53d0322c51ac816cea75bab86e8209a
jimmy789521 <71819826+jimmy789521@users.noreply.github.com>
2022-11-28 09:29:35 +0100

    Update covid-db.sql

--- covid-db.sql
+++ covid-db.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS CENTERS CASCADE;
 DROP TYPE IF EXISTS STATE_CENTER CASCADE;
-CREATE TYPE STATE_CENTER as ENUM('Open','Closed');
+CREATE TYPE STATE_CENTER as ENUM('Open','Closed','Disable');
 CREATE TABLE CENTERS(
 	center_id bigint PRIMARY KEY,
 	name varchar(50) not NULL,
@@ -8,11 +8,14 @@ CREATE TABLE CENTERS(
 	state STATE_CENTER not NULL,
 	city varchar(50) not NULL
 );
+DROP TYPE IF EXISTS STATE_ADMIN CASCADE;
+CREATE TYPE STATE_ADMIN as ENUM('Active','Disable');
 DROP TABLE IF EXISTS ADMINS;
 CREATE TABLE ADMINS(
 	admin_id bigint PRIMARY KEY,
 	firstName varchar(30) not NULL,
 	lastName varchar(30)not NULL,
+	state STATE_ADMIN not NULL,
 	center_id bigint,
 	FOREIGN KEY (center_id) REFERENCES CENTERS(center_id)
 );
@@ -21,11 +24,12 @@ CREATE TABLE DOCTORS(
 	doctor_id bigint PRIMARY KEY,
 	firstName varchar(30) not NULL,
 	lastName varchar(30) not NULL,
+	state STATE_ADMIN not NULL,
 	center_id bigint,
 	FOREIGN KEY (center_id) REFERENCES CENTERS(center_id)
 );
 DROP TYPE IF EXISTS STATE_CLIENT CASCADE;
-CREATE TYPE STATE_CLIENT as ENUM('Treated','Waiting');
+CREATE TYPE STATE_CLIENT as ENUM('Treated','Waiting','Cancelled','Disable');
 DROP TABLE IF EXISTS APPOINTMENTS;
 CREATE TABLE APPOINTMENTS (
 	appointment_id bigint PRIMARY KEY,
@@ -42,6 +46,7 @@ DROP TABLE IF EXISTS SUPERADMINS;
 CREATE TABLE SUPERADMINS(
 	superAdmin_id bigint PRIMARY KEY,
 	firstName varchar(30),
-	lastName varchar(30)
+	lastName varchar(30),
+	state STATE_ADMIN not NULL
 );
 select * from CENTERS;
