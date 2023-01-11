428fe45fda91fa23d5b1735a855534eff2dbd471
jimmy789521 <jimmy.bernabe6@etu.univ-lorraine.fr>
2023-01-11 12:32:16 +0100

    Added Appointment controller and various fixes

--- covid-api/src/main/java/org/polytech/covid/entities/Center.java
+++ covid-api/src/main/java/org/polytech/covid/entities/Center.java
@@ -1,16 +1,11 @@
 package org.polytech.covid.entities;
 
-
-import java.util.List;
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name="CENTERS")
@@ -19,29 +14,20 @@ public class Center{
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     @Column(nullable=false)
-    private Long center_id;
+    private Long id;
 
     private String location;
     private String name;
     private String state;
     private String city;
-
-
-    @OneToMany(cascade={CascadeType.ALL}, targetEntity=Doctor.class, mappedBy="center")
-    private List<Doctor> doctorList;
-
-    @OneToMany(cascade={CascadeType.REMOVE}, targetEntity=Admin.class, mappedBy="center")
-    private List<Admin> adminList;
-
-    @OneToMany(cascade={CascadeType.REMOVE}, targetEntity=Appointment.class, mappedBy="center")
-    private List<Appointment> appointmentList;
+    private boolean disabled;
 
     public Center(){
         
     }
 
-    public Long getCenter_id() {
-        return center_id;
+    public Long getId() {
+        return id;
     }
 
     public String getLocation() {
@@ -76,19 +62,12 @@ public class Center{
         this.city = city;
     }
 
-    @JsonIgnore
-    public List<Doctor> getDoctorList() {
-        return this.doctorList;
+    public boolean getDisabled (){
+        return disabled;
     }
 
-    @JsonIgnore
-    public List<Admin> getAdminList() {
-        return this.adminList;
-    }
-
-    @JsonIgnore
-    public List<Appointment> getAppointmentList() {
-        return this.appointmentList;
+    public void setDisabled (boolean disabled){
+        this.disabled = disabled;
     }
     
 }
