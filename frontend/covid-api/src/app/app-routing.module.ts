4c17ebd3f393bc5264e36f98365e3604f4778719
clement <clement.loth1@gmail.com>
2023-01-14 12:13:00 +0100

    first part for admin pages + angular material

--- frontend/covid-api/src/app/app-routing.module.ts
+++ frontend/covid-api/src/app/app-routing.module.ts
@@ -6,13 +6,17 @@ import { AppointmentComponent } from './appointment/appointment.component';
 import { FormLoginComponent } from './form-login/form-login.component';
 import { FormSigninComponent } from './form-signin/form-signin.component';
 import { AuthGuard } from './_guards/auth.guard';
+import { AdminComponent } from './admin/admin.component';
+import { FormCenterComponent } from './form-center/form-center.component';
 
 const routes: Routes = [
   {path: "centers", component: CenterComponent},
   {path: "appointment/:id", component: AppointmentComponent},
   {path: "", redirectTo: "/centers", pathMatch: "full"},
   {path: "login", component: FormLoginComponent},
-  {path: "signin", component: FormSigninComponent, canActivate:[AuthGuard]}
+  {path: "signin", component: FormSigninComponent, canActivate:[AuthGuard]},
+  {path: "admin", component: AdminComponent},
+  {path: "center/:id/edit", component:FormCenterComponent}
 ];
 
 @NgModule({
